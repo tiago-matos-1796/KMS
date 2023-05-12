64d021bfd2b55a5fd4929ab10251a235ddad9a71
Tiago <tiagomatos500@hotmail.com>
2023-05-12 18:21:26 +0100

    Add cors dependency

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 require('dotenv').config();
 
 const db = require('./models');
